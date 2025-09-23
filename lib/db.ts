0c0a96b5eb587e7a8cf86bff21c7891f27d4734b
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2025-09-23 16:38:06 +0530

    create interface for user

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import { error } from "console";
 import mongoose from "mongoose";
 
-const MONGODB_URL=process.env.MONGODB_URI
+const MONGODB_URL=process.env.MONGODB_URI!
 
 if(!MONGODB_URL){
   throw new Error("please define MongoDB URI in env file")
@@ -13,6 +13,24 @@ if(!cached){
   cached = global.mongoose= {conn:null,promise:null}
 }
 
-export async function ConnectTodatabase() {if(cached.conn)
-  return cached.conn
+export async function ConnectTodatabase() {if(cached.conn){
+    return cached.conn
+}
+
+if(!cached.promise){
+const opts= {
+  bufferCommands:true,
+maxPoolSize:10
+
+}
+
+cached.promise= mongoose.connect(MONGODB_URL,opts).then(()=>mongoose.connection);
+}
+try{
+cached.conn= await cached.promise
+}catch(error){
+cached.promise=null
+throw error
+}
+return cached.conn
 }
---
+++ modals/User.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+export interface IUser{
+  email:string,
+  password:string,
+  _id?:mongoose.Types.ObjectId,
+createdAt?:Date,
+updatedAt?:Date
+}
