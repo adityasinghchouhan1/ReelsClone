c54b0d6e9314bdb004b72d9950975454082f4b4c
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2025-09-23 17:59:42 +0530

    add User and Video models with schemas and interfaces

--- modals/User.ts
+++ modals/User.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { model ,models} from "mongoose";
 import bcrypt from "bcryptjs";
 
 
@@ -9,3 +9,29 @@ export interface IUser{
 createdAt?:Date,
 updatedAt?:Date
 }
+
+
+const UserSchema = new mongoose.Schema<IUser>({
+  email:{
+    type:String,
+    required:true,
+    unique:true,
+  },
+  password:{
+    type:String,
+    required:true,
+  },
+
+},{timestamps:true  },
+)
+
+UserSchema.pre("save",async function(next){
+  if(!this.isModified("password")){
+    this.password = await bcrypt.hash(this.password,10)
+}
+next()
+})
+
+const User =models?.User || model<IUser>("User",UserSchema)
+
+export default User
