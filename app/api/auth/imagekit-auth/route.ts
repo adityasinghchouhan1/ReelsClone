c5b497cf3ede74571763bb06b905d2750300b177
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2025-09-24 16:32:12 +0530

    add ImageKit authentication route and update dependencies

---
+++ app/api/auth/imagekit-auth/route.ts
@@ -0,0 +1,19 @@
+import { error } from "console";
+import ImageKit from "imagekit";
+import { NextResponse } from "next/server";
+
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+  privateKey: process.env.PRIVATE_KEY!,
+  urlEndpoint: process.env.NEXT_PUBLIC_ENDPOINT!,
+})
+
+export async function GET(){
+  try{
+    return NextResponse.json(imagekit.getAuthenticationParameters())
+
+  }
+  catch(error){
+    return NextResponse.json({error:"ImageKit auth failed"},{status:500})
+  }
+}
